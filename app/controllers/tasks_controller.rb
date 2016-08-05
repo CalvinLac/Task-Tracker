592402a990adb6ac09b270421b2558fd1e488905
Calvin <Calvin.lacc@gmail.com>
2016-08-04 19:14:30 -0600

    Imported prior work and finished the destroy function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,14 +10,32 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = Task.new(task_params)
+    @task = Task.new
   end
 
+  def create 
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      rerender new_task_path
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    if @task.destroy
+      redirect_to root_path
+    else
+      rerender task_path
+    end
+  end
 
 private
   def task_params
-      params.permit(:title, :description, :date)
+       params.require(:task).permit(:title, :description, :date)
   end
 
-  
+
 end
